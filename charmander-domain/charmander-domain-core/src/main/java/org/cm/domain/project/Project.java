71abc86a499581ca15816c568888b3386ad4ea70
devyoon <wideskyinme@gmail.com>
2025-02-28 18:34:40 +0900

    feat: Project 엔티티 정의

---
+++ charmander-domain/charmander-domain-core/src/main/java/org/cm/domain/project/Project.java
@@ -0,0 +1,56 @@
+package org.cm.domain.project;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Version;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.cm.domain.common.BaseEntity;
+import org.cm.domain.common.Member;
+
+@Getter
+@Entity(name = "project")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Project extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, updatable = false)
+    private Member owner;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ProjectStatus status = ProjectStatus.DRAFT;
+
+    @Column(nullable = false, columnDefinition = "MEDIUMTEXT")
+    private String data;
+
+    @Version
+    @Column(nullable = false)
+    private int version;
+
+    @Column(nullable = false)
+    private LocalDateTime lastAccessedAt = LocalDateTime.now();
+
+    public Project(
+            Member owner,
+            String name,
+            ProjectStatus status,
+            String data,
+            int version
+    ) {
+        this.owner = owner;
+        this.name = name;
+        this.status = status;
+        this.data = data;
+        this.version = version;
+    }
+}
