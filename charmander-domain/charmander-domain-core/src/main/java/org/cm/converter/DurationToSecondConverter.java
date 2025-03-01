dad24a91d12e08a8d7655369cf66eb74628e2531
devyoon <wideskyinme@gmail.com>
2025-03-01 16:44:21 +0900

    refactor: @Converter 추가

--- charmander-domain/charmander-domain-core/src/main/java/org/cm/converter/DurationToSecondConverter.java
+++ charmander-domain/charmander-domain-core/src/main/java/org/cm/converter/DurationToSecondConverter.java
@@ -1,9 +1,11 @@
 package org.cm.converter;
 
 import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
+@Converter
 public class DurationToSecondConverter implements AttributeConverter<Duration, Integer> {
     @Override
     public Duration convertToEntityAttribute(Integer dbData) {
