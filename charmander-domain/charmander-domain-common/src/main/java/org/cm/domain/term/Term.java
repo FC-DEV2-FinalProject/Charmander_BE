01eee91c2f1b6e6779ef3e924e84779d54d3b512
devyoon <wideskyinme@gmail.com>
2025-03-01 20:29:49 +0900

    refactor: Term 엔티티 리팩토링

--- charmander-domain/charmander-domain-common/src/main/java/org/cm/domain/term/Term.java
+++ charmander-domain/charmander-domain-common/src/main/java/org/cm/domain/term/Term.java
@@ -4,19 +4,31 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Lob;
 import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.cm.domain.common.BaseEntity;
 
+@Getter
 @Entity(name = "terms")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Term extends BaseEntity {
-
-    @Column(name = "terms_type")
+    @Column(nullable = false)
     private String type;
+
+    @Column(nullable = false)
     private String version;
+
     @Lob
-    @Column(columnDefinition = "TEXT")
+    @Column(nullable = false, columnDefinition = "MEDIUMTEXT")
     private String content;
+
+    @Column(nullable = false)
     private LocalDate effectiveDate;
+
+    @Column(nullable = false)
     private boolean mandatory;
+
+    @Column(nullable = false)
     private int priority;
 }
-
