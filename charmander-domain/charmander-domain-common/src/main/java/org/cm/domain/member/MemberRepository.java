f5bf52b2eb0f9d9b28a10ae852e5bbb3d6780ff9
dudlf <dudlf016@gmail.com>
2025-03-06 16:01:44 +0900

    feat: 로그인 (JWT 사용)

--- charmander-domain/charmander-domain-common/src/main/java/org/cm/domain/member/MemberRepository.java
+++ charmander-domain/charmander-domain-common/src/main/java/org/cm/domain/member/MemberRepository.java
@@ -1,6 +1,9 @@
 package org.cm.domain.member;
 
+import org.jspecify.annotations.Nullable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    @Nullable
+    Member findByPrincipal_IdAndPrincipal_Type(String username, MemberPrincipalType memberPrincipalType);
 }
