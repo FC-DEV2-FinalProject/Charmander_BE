c7cb9d625f6a0fdceae81e3a2e977c0a1604e36c
dudlf <dudlf016@gmail.com>
2025-03-06 16:03:24 +0900

    feat: 로그아웃 (JWT 사용)

--- charmander-api/charmander-api-core/src/main/java/org/cm/api/auth/AuthController.java
+++ charmander-api/charmander-api-core/src/main/java/org/cm/api/auth/AuthController.java
@@ -1,16 +1,19 @@
 package org.cm.api.auth;
 
 import jakarta.servlet.http.HttpServletResponse;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import org.cm.api.auth.dto.LoginRequest;
 import org.cm.api.auth.dto.LoginResponse;
 import org.cm.jwt.JwtConstants;
 import org.cm.jwt.JwtProperty;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,6 +31,13 @@ public class AuthController {
         return loginResponse;
     }
 
+    @PostMapping("/logout")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void logout(HttpServletResponse servletResponse) {
+        authService.logout();
+        revokeRefreshTokenCookie(servletResponse);
+    }
+
     private void addRefreshTokenCookie(HttpServletResponse servletResponse, String token) {
         var cookie = ResponseCookie
             .from(JwtConstants.REFRESH_TOKEN_COOKIE_NAME)
@@ -37,6 +47,19 @@ public class AuthController {
             .domain(jwtProperty.getCookie().getDomain())
             .path(JwtConstants.REFRESH_TOKEN_COOKIE_PATH)
             .maxAge(JwtConstants.REFRESH_TOKEN_DURATION)
+            .sameSite(JwtConstants.REFRESH_TOKEN_COOKIE_SAME_SITE)
+            .build();
+        servletResponse.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+    }
+
+    private void revokeRefreshTokenCookie(HttpServletResponse servletResponse) {
+        var cookie = ResponseCookie
+            .from(JwtConstants.REFRESH_TOKEN_COOKIE_NAME)
+            .secure(true)
+            .httpOnly(true)
+            .domain(jwtProperty.getCookie().getDomain())
+            .path("/")
+            .maxAge(Duration.ZERO)
             .sameSite(JwtConstants.REFRESH_TOKEN_COOKIE_SAME_SITE)
             .build();
         servletResponse.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
