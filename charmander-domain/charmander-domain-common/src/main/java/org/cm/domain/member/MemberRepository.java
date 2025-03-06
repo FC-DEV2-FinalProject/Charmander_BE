0d2a3bd567d7e7f3719afcc1eb4cb54776c107b8
dudlf <dudlf016@gmail.com>
2025-03-06 16:11:18 +0900

    feat: 회원 가입 시 이메일 인증 (Redis 사용)

--- charmander-domain/charmander-domain-common/src/main/java/org/cm/domain/member/MemberRepository.java
+++ charmander-domain/charmander-domain-common/src/main/java/org/cm/domain/member/MemberRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     @Nullable
     Member findByPrincipal_IdAndPrincipal_Type(String username, MemberPrincipalType memberPrincipalType);
+
+    boolean existsByPrincipal_IdAndPrincipal_Type(String email, MemberPrincipalType memberPrincipalType);
 }
