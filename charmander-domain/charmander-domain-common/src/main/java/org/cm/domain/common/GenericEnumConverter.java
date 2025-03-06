0967518dd357c848daa94f6d96764830700aad40
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-03-06 16:14:38 +0900

    refactor: enum converter 에서 커스텀 예외를 사용하도록 변경

--- charmander-domain/charmander-domain-common/src/main/java/org/cm/domain/common/GenericEnumConverter.java
+++ charmander-domain/charmander-domain-common/src/main/java/org/cm/domain/common/GenericEnumConverter.java
@@ -1,7 +1,11 @@
 package org.cm.domain.common;
 
 import jakarta.persistence.AttributeConverter;
+import lombok.extern.slf4j.Slf4j;
+import org.cm.domain.exception.CommonDomainException;
+import org.cm.domain.exception.CommonDomainExceptionCode;
 
+@Slf4j
 @SuppressWarnings("ConverterNotAnnotatedInspection")
 public abstract class GenericEnumConverter<T extends Enum<T> & PersistenceEnum<E>, E> implements
     AttributeConverter<T, E> {
@@ -24,6 +28,7 @@ public abstract class GenericEnumConverter<T extends Enum<T> & PersistenceEnum<E
                 return e;
             }
         }
-        throw new IllegalArgumentException("Unknown value: " + dbData);
+        log.error("unknown value: {}", dbData);
+        throw new CommonDomainException(CommonDomainExceptionCode.ELEMENT_NOT_FOUND);
     }
 }
