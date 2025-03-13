38f1db027f0f54185ac8d59911ca287b9c149fa7
dudlf <dudlf016@gmail.com>
2025-03-13 18:55:31 +0900

    feat: CoreApiExceptionCode에 WebExceptionCode 사용

--- charmander-api/charmander-api-core/src/main/java/org/cm/exception/CoreApiExceptionCode.java
+++ charmander-api/charmander-api-core/src/main/java/org/cm/exception/CoreApiExceptionCode.java
@@ -2,12 +2,14 @@ package org.cm.exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.cm.common.exception.ExceptionCode;
+import org.cm.common.exception.WebExceptionCode;
 import org.springframework.boot.logging.LogLevel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @Getter
 @RequiredArgsConstructor
-public enum CoreApiExceptionCode implements ExceptionCode {
+public enum CoreApiExceptionCode implements WebExceptionCode {
     // @formatter:off
     ACCOUNT_EMAIL_ALREADY_IN_USE("A001", "Email is already in use."),
     ACCOUNT_VERIFICATION_CODE_NOT_MATCH("A002", "Verification code does not match."),
@@ -16,7 +18,7 @@ public enum CoreApiExceptionCode implements ExceptionCode {
 
     MEMBER_NOT_FOUND("M001", "Member not found."),
 
-    PROJECT_NOT_FOUND("P001", "Project not found."),
+    PROJECT_NOT_FOUND("P001", "Project not found.", HttpStatus.NOT_FOUND),
 
     TASK_NOT_FOUND("T001", "Task not found."),
 
@@ -30,16 +32,30 @@ public enum CoreApiExceptionCode implements ExceptionCode {
     private final String code;
     private final String message;
     private final LogLevel level;
+    private final HttpStatusCode httpStatusCode;
 
     // 예외 코드의 기본 설정은 INFO
     CoreApiExceptionCode(String code, String message) {
         this.code = code;
         this.message = message;
         this.level = LogLevel.INFO;
+        this.httpStatusCode = HttpStatus.BAD_REQUEST;
+    }
+
+    CoreApiExceptionCode(String code, String message, HttpStatusCode httpStatusCode) {
+        this.code = code;
+        this.message = message;
+        this.level = LogLevel.INFO;
+        this.httpStatusCode = httpStatusCode;
     }
 
     @Override
     public String getPrefix() {
         return "CORAPI";
+    }
+
+    @Override
+    public HttpStatusCode httpStatusCode() {
+        return httpStatusCode;
     }
 }
