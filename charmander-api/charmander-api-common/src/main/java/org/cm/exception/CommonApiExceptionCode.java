91c79844e25df8dcc36c033f02e07659edf2f1e2
dudlf <dudlf016@gmail.com>
2025-03-13 15:45:53 +0900

    feat: Common API 공통 예외

---
+++ charmander-api/charmander-api-common/src/main/java/org/cm/exception/CommonApiExceptionCode.java
@@ -0,0 +1,34 @@
+package org.cm.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.cm.common.exception.ExceptionCode;
+import org.springframework.boot.logging.LogLevel;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommonApiExceptionCode implements ExceptionCode {
+    // @formatter:off
+
+    ;
+    // @formatter:on
+
+    // Core domain
+    private static final String PREFIX = "COAPI";
+
+    private final String code;
+    private final String message;
+    private final LogLevel level;
+
+    // 예외 코드의 기본 설정은 INFO
+    CommonApiExceptionCode(String code, String message) {
+        this.code = code;
+        this.message = message;
+        this.level = LogLevel.INFO;
+    }
+
+    @Override
+    public String getPrefix() {
+        return "COAPI";
+    }
+}
