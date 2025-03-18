08bf887fcb76c737ff33108fdb0854f6bcaef331
devyoon <wideskyinme@gmail.com>
2025-03-18 11:35:23 +0900

    refactor: Project 엔티티 수정 - ProjectStatus 관련 컬럼 및 클래스 삭제

--- charmander-domain/charmander-domain-core/src/main/java/org/cm/domain/project/Project.java
+++ charmander-domain/charmander-domain-core/src/main/java/org/cm/domain/project/Project.java
@@ -1,12 +1,10 @@
 package org.cm.domain.project;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Version;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -28,10 +26,6 @@ public class Project extends BaseEntity {
     @Column(nullable = false)
     private String name;
 
-    @Convert(converter = ProjectStatusConverter.class)
-    @Column(nullable = false)
-    private ProjectStatus status = ProjectStatus.DRAFT;
-
     @Setter
     @Column(nullable = false, columnDefinition = "MEDIUMTEXT")
     private String data;
@@ -46,18 +40,16 @@ public class Project extends BaseEntity {
     public Project(
         Member owner,
         String name,
-        ProjectStatus status,
         String data,
         int version
     ) {
         this.owner = owner;
         this.name = name;
-        this.status = status;
         this.data = data;
         this.version = version;
     }
 
-    public static Project newDraftProject(Member owner) {
-        return new Project(owner, "New Project", ProjectStatus.DRAFT, "{}", 0);
+    public static Project newProject(Member owner) {
+        return new Project(owner, "New Project", "{}", 0);
     }
 }
