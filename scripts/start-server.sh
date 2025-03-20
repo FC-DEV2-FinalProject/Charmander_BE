fde0ca3c2beef85beec09b1328a161507cc658a2
devyoon <wideskyinme@gmail.com>
2025-03-20 11:20:43 +0900

    docs: CICD scripts 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop charmander-api-server-ecr || true
 docker rm charmander-api-server-ecr || true
-docker pull {536697268731.dkr.ecr.ap-northeast-2.amazonaws.com/charmander-api-server-ecr}/charmander-api-server-ecr:latest
-docker run -d --name charmander-dev-server-ecr -p 8080:8080 {536697268731.dkr.ecr.ap-northeast-2.amazonaws.com/charmander-api-server-ecr}/charmander-api-server-ecr:latest
+docker pull 536697268731.dkr.ecr.ap-northeast-2.amazonaws.com/charmander-api-server-ecr/charmander-api-server-ecr:latest
+docker run -d --name charmander-api-server-ecr -p 8080:8080 536697268731.dkr.ecr.ap-northeast-2.amazonaws.com/charmander-api-server-ecr/charmander-api-server-ecr:latest
 echo "--------------- 서버 배포 끝 -----------------"
