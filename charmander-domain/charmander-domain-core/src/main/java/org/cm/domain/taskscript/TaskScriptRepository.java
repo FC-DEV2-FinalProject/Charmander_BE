7bca9011ca912f6ff2224a33b08c13e66afdd6ff
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-03-20 17:44:17 +0900

    feat: 작업 스크립트 엔티티 정의

---
+++ charmander-domain/charmander-domain-core/src/main/java/org/cm/domain/taskscript/TaskScriptRepository.java
@@ -0,0 +1,7 @@
+package org.cm.domain.taskscript;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskScriptRepository extends JpaRepository<TaskScript, Long> {
+
+}
