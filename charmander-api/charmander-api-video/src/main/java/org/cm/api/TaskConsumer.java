fbfd1f461f2cca98b068c317051bb026580aa37d
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-03-21 19:50:24 +0900

    refactor: 컨슈머 코드 메서드 분리

--- charmander-api/charmander-api-video/src/main/java/org/cm/api/TaskConsumer.java
+++ charmander-api/charmander-api-video/src/main/java/org/cm/api/TaskConsumer.java
@@ -4,12 +4,14 @@ package org.cm.api;
 import lombok.RequiredArgsConstructor;
 import org.cm.infra.storage.ContentsLocator;
 import org.cm.infra.storage.PreSignedFileUploadService;
+import org.cm.infra.storage.PreSignedURLIdentifier;
 import org.cm.infra.storage.SingleFileUploadService;
 import org.cm.service.TtsService;
 import org.cm.vo.TaskCompletedCheckEvent;
 import org.cm.vo.TaskScriptGenerationCompletedEvent;
 import org.cm.vo.TaskScriptGenerationStartedEvent;
 import org.cm.vo.TtsCreateCommand;
+import org.cm.vo.TtsInfo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -30,14 +32,33 @@ public class TaskConsumer {
 
     @KafkaListener(topics = "tts-task", groupId = "tts-task-group")
     public void consume(TaskScriptRecord taskScriptRecord) {
-        var taskScriptGenerationStartedEvent = new TaskScriptGenerationStartedEvent(
-                taskScriptRecord.taskId(),
-                taskScriptRecord.taskScriptId()
-        );
-
-        applicationEventPublisher.publishEvent(taskScriptGenerationStartedEvent);
+        prepareTaskScriptGeneration(taskScriptRecord);
 
         var preSignedURLIdentifier = preSignedFileUploadService.sign(contentsLocator);
+        var ttsInfo = createTts(taskScriptRecord, preSignedURLIdentifier);
+
+        completeTaskScriptGeneration(taskScriptRecord, preSignedURLIdentifier, ttsInfo);
+    }
+
+    private void completeTaskScriptGeneration(
+            TaskScriptRecord taskScriptRecord,
+            PreSignedURLIdentifier preSignedURLIdentifier,
+            TtsInfo ttsInfo
+    ) {
+        var taskScriptGenerationCompletedEvent = new TaskScriptGenerationCompletedEvent(
+                taskScriptRecord.taskId(),
+                taskScriptRecord.taskScriptId(),
+                preSignedURLIdentifier.fileName(),
+                ttsInfo.playTime()
+        );
+        applicationEventPublisher.publishEvent(taskScriptGenerationCompletedEvent);
+        applicationEventPublisher.publishEvent(new TaskCompletedCheckEvent(taskScriptRecord.taskId()));
+    }
+
+    private TtsInfo createTts(
+            TaskScriptRecord taskScriptRecord,
+            PreSignedURLIdentifier preSignedURLIdentifier
+    ) {
         var ttsCreateCommand = new TtsCreateCommand(
                 taskScriptRecord.sentence(),
                 taskScriptRecord.option(),
@@ -46,16 +67,14 @@ public class TaskConsumer {
                 preSignedURLIdentifier.fileName(),
                 preSignedURLIdentifier.uploadId()
         );
+        return ttsService.create(ttsCreateCommand);
+    }
 
-        var ttsInfo = ttsService.create(ttsCreateCommand);
-        var taskScriptGenerationCompletedEvent = new TaskScriptGenerationCompletedEvent(
+    private void prepareTaskScriptGeneration(TaskScriptRecord taskScriptRecord) {
+        var taskScriptGenerationStartedEvent = new TaskScriptGenerationStartedEvent(
                 taskScriptRecord.taskId(),
-                taskScriptRecord.taskScriptId(),
-                preSignedURLIdentifier.fileName(),
-                ttsInfo.playTime()
+                taskScriptRecord.taskScriptId()
         );
-
-        applicationEventPublisher.publishEvent(taskScriptGenerationCompletedEvent);
-        applicationEventPublisher.publishEvent(new TaskCompletedCheckEvent(taskScriptRecord.taskId()));
+        applicationEventPublisher.publishEvent(taskScriptGenerationStartedEvent);
     }
 }
