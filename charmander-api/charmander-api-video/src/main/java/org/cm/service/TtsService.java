53f6f78bdfb72f45299ebd357bebfa14c81b8934
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-03-21 19:04:23 +0900

    feat: tts 요청 기능 추가

--- charmander-api/charmander-api-video/src/main/java/org/cm/service/TtsService.java
+++ charmander-api/charmander-api-video/src/main/java/org/cm/service/TtsService.java
@@ -1,6 +1,7 @@
 package org.cm.service;
 
 import org.cm.vo.TtsCreateCommand;
+import org.cm.vo.TtsInfo;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.HttpExchange;
@@ -8,9 +9,9 @@ import org.springframework.web.service.annotation.PostExchange;
 
 @Component
 @HttpExchange
-public interface TtsService{
+public interface TtsService {
 
-    @PostExchange("/tts")
-    void create(@RequestBody TtsCreateCommand command);
+    @PostExchange("/api/speak/v1")
+    TtsInfo create(@RequestBody TtsCreateCommand command);
 
 }
