645fb682c52d97340cea366f49fecc8d910fdd2d
jaho96 <jaho321@naver.com>
2025-03-26 20:59:44 +0900

    refactor: aop를 통해 service단 Project Article 수정 로직 간소화

--- charmander-api/charmander-api-core/src/main/java/org/cm/api/project/ProjectService.java
+++ charmander-api/charmander-api-core/src/main/java/org/cm/api/project/ProjectService.java
@@ -3,6 +3,7 @@ package org.cm.api.project;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.cm.api.task.TaskService;
+import org.cm.domain.common.NamedLockRepository;
 import org.cm.domain.member.MemberRepository;
 import org.cm.domain.project.Project;
 import org.cm.domain.project.ProjectRepository;
@@ -36,6 +37,7 @@ public class ProjectService implements ApplicationEventPublisherAware {
 
     private final MemberRepository memberRepository;
     private final ProjectRepository projectRepository;
+    private final NamedLockRepository namedLockRepository;
 
     private static final Logger logger = LoggerFactory.getLogger(ProjectService.class);
 
@@ -68,64 +70,17 @@ public class ProjectService implements ApplicationEventPublisherAware {
     // modify
     @Transactional
     public void modifyProjectNewsArticle(Long id, Long memberId, String newArticle, LocalDateTime timestamp) {
-        int retryCount = 0;
-        int maxRetries = 5;
+        Project foundProject = projectRepository
+            .findByIdAndOwnerIdForUpdate(id, memberId)
+            .orElseThrow(() -> new CoreDomainException(CoreDomainExceptionCode.NOT_FOUND_PROJECT));
 
-        while (retryCount < maxRetries) {
-            try {
-                Project foundProject = projectRepository
-                    .findByIdAndOwnerIdForUpdate(id, memberId)
-                    .orElseThrow(() -> new CoreDomainException(CoreDomainExceptionCode.NOT_FOUND_PROJECT));
+        // db에 저장된 updateAt보다 timestamp가 최신 정보를 가지고 있다면 충돌 발생
+        if (!foundProject.getUpdatedAt().isEqual(timestamp) && foundProject.getUpdatedAt().isAfter(timestamp)) {
+            throw new OptimisticLockingFailureException("동시성 충돌: 다른 사용자가 이미 수정했습니다.");
+        }
 
-                // db에 저장된 updateAt보다 timestamp가 최신 정보를 가지고 있다면 충돌 발생
-                if (!foundProject.getUpdatedAt().isEqual(timestamp) && foundProject.getUpdatedAt().isAfter(timestamp)) {
-                    throw new OptimisticLockingFailureException("동시성 충돌: 다른 사용자가 이미 수정했습니다.");
-                }
-
-                foundProject.updateProjectNewsArticle(newArticle);
-                projectRepository.save(foundProject);
-
-                break; // 성공적으로 업데이트 완료
-
-            } catch (OptimisticLockingFailureException e) {
-                retryCount++;
-                logger.warn("데이터 충돌 발생 {}", retryCount);
-
-                // 잠시 대기 후 재시도
-                try {
-                    Thread.sleep(300);
-                } catch (InterruptedException ex) {
-                    Thread.currentThread().interrupt();
-                }
-
-                if (retryCount >= maxRetries) {
-                    logger.info("동시성 5회 이상 충돌 ");
-                    logger.info("네임드 락 시작");
-
-                    boolean lockAcquired  = projectRepository.getLock(id.toString());
-
-                    if (!lockAcquired) {
-                        logger.error("네임드 락 획득 실패: id={}", id);
-                        throw new OptimisticLockingFailureException("네임드 락을 획득할 수 없습니다. 잠시 후 다시 시도해주세요.");
-                    }
-
-                    try {
-                        projectRepository.updateProjectNewsArticleFindById(id, newArticle, timestamp);
-                    } catch (Exception ex) {
-                        logger.error("네임드 락 기반 업데이트 실패: {}", ex.getMessage(), ex);
-                        throw new RuntimeException("네임드 락을 활용한 업데이트 중 오류가 발생했습니다.", e);
-                    } finally {
-                        projectRepository.releaseLock(id.toString());
-                        logger.info("네임드 락 종료");
-                    }
-
-                    break;
-                } // end if
-            } catch (Exception e) {
-                logger.error("예기치 못한 오류 발생: {}", e.getMessage(), e);
-                throw new RuntimeException("예기치 못한 오류로 인해 업데이트에 실패했습니다.", e);
-            }
-        }// end while
+        foundProject.updateProjectNewsArticle(newArticle);
+        projectRepository.save(foundProject);
     }// end method
 
     public Task generateProject(@NonNull AuthInfo authInfo, Long id) {
