716fcd0a5d65af481253e44e6fa15e49babbc85a
dudlf <dudlf016@gmail.com>
2025-03-27 16:55:12 +0900

    style: Repository Query 문자열 변경

--- charmander-domain/charmander-domain-core/src/main/java/org/cm/domain/scene/SceneRepository.java
+++ charmander-domain/charmander-domain-core/src/main/java/org/cm/domain/scene/SceneRepository.java
@@ -9,23 +9,25 @@ import java.util.List;
 import java.util.Optional;
 
 public interface SceneRepository extends JpaRepository<Scene, Long> {
-    @Query(
-        "SELECT s FROM Scene s " +
-        "JOIN s.project p " +
-        "JOIN p.owner m " +
-        "WHERE p.id = :projectId " +
-        "AND m.id = :memberId"
-    )
+    @Query("""
+          SELECT s
+          FROM Scene s
+          JOIN s.project p
+          JOIN p.owner m
+          WHERE p.id = :projectId
+            AND m.id = :memberId
+      """)
     List<Scene> findProjectScenes(Long projectId, Long memberId);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query(
-      "SELECT s FROM Scene s " +
-        "JOIN s.project p " +
-        "JOIN p.owner m " +
-        "WHERE p.id = :projectId " +
-        "AND s.id = :sceneId " +
-        "AND m.id = :memberId"
-    )
+    @Query("""
+          SELECT s
+          FROM Scene s
+          JOIN s.project p
+          JOIN p.owner m
+          WHERE p.id = :projectId
+          AND s.id = :sceneId
+          AND m.id = :memberId
+      """)
     Optional<Scene> findProjectSceneForUpdate(Long projectId, Long sceneId, Long memberId);
 }
