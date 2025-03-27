9e189cf3eae9055451771ec6a2df6098e6ad3ee0
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-03-27 22:39:07 +0900

    feat: 영상 오버레이 컨슈머 기능 구현

---
+++ charmander-api/charmander-api-video/src/main/java/org/cm/api/SceneCombineConsumer.java
@@ -0,0 +1,47 @@
+package org.cm.api;
+
+import lombok.RequiredArgsConstructor;
+import org.cm.common.utils.RandomKeyGenerator;
+import org.cm.domain.task.SceneOutputRepository;
+import org.cm.infra.mediaconvert.queue.VideoOverlayCombineQueue;
+import org.cm.infra.mediaconvert.queue.WavVideoCombineQueue.VideoSource;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SceneCombineConsumer {
+
+    private final SceneOutputRepository sceneOutputRepository;
+    private final VideoOverlayCombineQueue videoOverlayCombineQueue;
+
+    @KafkaListener(topics = "scene-task", groupId = "tts-task-group")
+    public void consume(SceneCombineRecord sceneCombineRecord) {
+        var sceneOutput = sceneOutputRepository.getById(sceneCombineRecord.sceneId());
+        var scene = sceneOutput.getScene();
+        var background = scene.background();
+        var avatar = scene.avatar();
+
+        videoOverlayCombineQueue.offer(
+                new VideoSource(
+                        sceneCombineRecord.videoId(),
+                        background.width(),
+                        background.height(),
+                        0,
+                        0
+                ),
+                new VideoSource(
+                        avatar.url(),
+                        1280,
+                        720,
+                        avatar.position().x(),
+                        avatar.position().y()
+                ),
+                RandomKeyGenerator.generateRandomKey()
+        );
+
+
+    }
+
+
+}
---
+++ charmander-api/charmander-api-video/src/main/java/org/cm/api/SceneCombineRecord.java
@@ -0,0 +1,8 @@
+package org.cm.api;
+
+public record SceneCombineRecord(
+        Long taskId,
+        Long sceneId,
+        String videoId
+) {
+}
