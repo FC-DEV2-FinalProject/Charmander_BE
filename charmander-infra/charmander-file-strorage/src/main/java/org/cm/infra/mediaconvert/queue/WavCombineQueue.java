05c4989f4dd4f4c3b5317ff1191414b3fb24b0f8
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-03-29 16:58:55 +0900

    refactor: tts 파일은 contentsLocator로 파일을 가져오도록 변경

--- charmander-infra/charmander-file-strorage/src/main/java/org/cm/infra/mediaconvert/queue/WavCombineQueue.java
+++ charmander-infra/charmander-file-strorage/src/main/java/org/cm/infra/mediaconvert/queue/WavCombineQueue.java
@@ -2,6 +2,7 @@ package org.cm.infra.mediaconvert.queue;
 
 import java.util.List;
 import java.util.Map;
+import org.cm.infra.storage.ContentsLocator;
 import software.amazon.awssdk.services.mediaconvert.model.AudioDefaultSelection;
 import software.amazon.awssdk.services.mediaconvert.model.AudioSelector;
 import software.amazon.awssdk.services.mediaconvert.model.Input;
@@ -21,6 +22,28 @@ public interface WavCombineQueue {
                     .audioSelectors(Map.of("Audio Selector 1", AudioSelector.builder()
                             .defaultSelection(AudioDefaultSelection.DEFAULT)
                             .externalAudioFileInput(s3AudioLocation)
+                            .build()))
+                    .videoSelector(VideoSelector.builder().build())
+                    .timecodeSource(InputTimecodeSource.ZEROBASED)
+                    .build();
+            if (duration >= 50) {
+                var delayInput = Input.builder()
+                        .audioSelectors(Map.of("Audio Selector 1", AudioSelector.builder()
+                                .defaultSelection(AudioDefaultSelection.DEFAULT)
+                                .build()))
+                        .videoSelector(VideoSelector.builder().build())
+                        .videoGenerator(InputVideoGenerator.builder().duration(duration).build())
+                        .build();
+                return List.of(audioInput, delayInput);
+            }
+            return List.of(audioInput);
+        }
+
+        public List<Input> toInput(ContentsLocator contentsLocator) {
+            var audioInput = Input.builder()
+                    .audioSelectors(Map.of("Audio Selector 1", AudioSelector.builder()
+                            .defaultSelection(AudioDefaultSelection.DEFAULT)
+                            .externalAudioFileInput(contentsLocator.combineLocation(s3AudioLocation))
                             .build()))
                     .videoSelector(VideoSelector.builder().build())
                     .timecodeSource(InputTimecodeSource.ZEROBASED)
