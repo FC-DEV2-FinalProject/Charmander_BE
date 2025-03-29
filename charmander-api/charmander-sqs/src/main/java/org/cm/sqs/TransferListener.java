644d83504e3e0c949122d03345fdfec3b43774f9
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-03-29 17:03:30 +0900

    refactor: 카프카에서 사용하는 토픽의 이름 변경

--- charmander-api/charmander-sqs/src/main/java/org/cm/sqs/TransferListener.java
+++ charmander-api/charmander-sqs/src/main/java/org/cm/sqs/TransferListener.java
@@ -1,14 +1,18 @@
 package org.cm.sqs;
 
 import io.awspring.cloud.sqs.annotation.SqsListener;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
-import org.cm.kafka.OverlayCompleteRecord;
-import org.cm.kafka.SceneCombineRecord;
-import org.cm.kafka.TtsCombineRecord;
-import org.cm.kafka.VideoConcatRecord;
+import lombok.extern.slf4j.Slf4j;
+import org.cm.kafka.CompletionNotificationRecord;
+import org.cm.kafka.KafkaTopicNames;
+import org.cm.kafka.SceneProcessingRecord;
+import org.cm.kafka.VideoCompositionRecord;
+import org.cm.kafka.VideoOverlayRecord;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class TransferListener {
@@ -19,6 +23,12 @@ public class TransferListener {
     public void messageListener(MediaConvertJobMessage message) {
         var detail = message.detail();
         var metadata = detail.userMetadata();
+
+        if (Objects.equals(detail.status(), "ERROR")) {
+            log.info("error");
+            return;
+        }
+        log.info(">>>>>> {}", message);
         var file = detail.outputGroupDetails()
                 .getFirst()
                 .outputDetails()
@@ -26,30 +36,51 @@ public class TransferListener {
                 .outputFilePaths()
                 .getFirst();
 
-        switch (detail.userMetadata().type()) {
-            case TTS_COMBINE -> kafkaTemplate.send("tts-combine-task", new TtsCombineRecord(
-                    metadata.taskId(),
-                    metadata.taskScriptId(),
-                    metadata.sceneId(),
-                    file
-            ));
-            case VIDEO_OVERLAY -> kafkaTemplate.send("video-overlay-task", new SceneCombineRecord(
-                    metadata.taskId(),
-                    metadata.taskScriptId(),
-                    metadata.sceneId(),
-                    file
-            ));
-            case SCENE_COMBINE -> kafkaTemplate.send("video-concat-task", new OverlayCompleteRecord(
-                    metadata.taskId(),
-                    metadata.sceneId(),
-                    file
-            ));
-            case CONCAT_VIDEO -> kafkaTemplate.send("scene-task", new VideoConcatRecord(
-                    metadata.taskId(),
-                    file
-            ));
+        if (detail.userMetadata().type() == null) {
+            return;
         }
 
-    }
+        log.info("{} {}", detail.jobId(), detail.userMetadata());
 
+        // tts 결합
+        // scene 결합
+        // scene - avatar 결합
+        // video 결합
+
+        switch (detail.userMetadata().type()) {
+            case TTS_COMBINE -> kafkaTemplate.send(
+                    KafkaTopicNames.SCENE_PROCESSING,
+                    new SceneProcessingRecord(
+                            metadata.taskId(),
+                            metadata.taskScriptId(),
+                            metadata.sceneId(),
+                            file
+                    )
+            );
+            case SCENE_COMBINE -> kafkaTemplate.send(
+                    KafkaTopicNames.OVERLAY_PROCESSING,
+                    new VideoOverlayRecord(
+                            metadata.taskId(),
+                            metadata.taskScriptId(),
+                            metadata.sceneId(),
+                            file
+                    )
+            );
+            case VIDEO_OVERLAY -> kafkaTemplate.send(
+                    KafkaTopicNames.VIDEO_COMPOSITION,
+                    new VideoCompositionRecord(
+                            metadata.taskId(),
+                            metadata.sceneId(),
+                            file
+                    )
+            );
+            case CONCAT_VIDEO -> kafkaTemplate.send(
+                    KafkaTopicNames.COMPLETION_NOTIFICATION,
+                    new CompletionNotificationRecord(
+                            metadata.taskId(),
+                            file
+                    )
+            );
+        }
+    }
 }
