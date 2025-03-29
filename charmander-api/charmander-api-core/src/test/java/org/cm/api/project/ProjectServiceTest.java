ed85422ee53ae4e34710f04d331950b1cd7d4612
dudlf <dudlf016@gmail.com>
2025-03-29 16:44:26 +0900

    test: Project Name을 변경할 수 있어야 함

--- charmander-api/charmander-api-core/src/test/java/org/cm/api/project/ProjectServiceTest.java
+++ charmander-api/charmander-api-core/src/test/java/org/cm/api/project/ProjectServiceTest.java
@@ -61,6 +61,30 @@ class ProjectServiceTest {
             assertThat(prevName).isEqualTo(afterName);
         }
 
+        @Test
+        @DisplayName("002. name을 변경할 수 있어야 함")
+        public void name을_변경할_수_있어야_함() {
+            // given
+            var authInfo = Mockito.mock(AuthInfo.class);
+            var projectId = 1L;
+            var request = new ProjectUpdateRequest(
+                "new name"
+            );
+            var project = ProjectFixture.create();
+            Mockito
+                .when(projectRepository.findByIdAndOwnerId(Mockito.anyLong(), Mockito.anyLong()))
+                .thenReturn(Optional.of(project));
 
+            // when
+            var prevName = project.getName();
+            projectService.updateProject(authInfo, projectId, request);
+            var afterName = project.getName();
+
+            // then
+            assertThat(request.name()).isNotNull();
+            assertThat(prevName).isNotEqualTo(request.name());
+            assertThat(prevName).isNotEqualTo(afterName);
+            assertThat(request.name()).isEqualTo(afterName);
+        }
     }
 }
